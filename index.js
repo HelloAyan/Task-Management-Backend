caa041fc17766f2607d9f9fee730ccd86b04d6e9
HelloAyan <helloayan171@gmail.com>
2026-02-19 17:02:06 +0600

    project setup and database connected

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+
+dotenv.config()
+const app = express()
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URL).then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+}).catch((error) => {
+    console.log("Error connecting to MongoDB:", error);
+})
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Task Management API is running');
+})
