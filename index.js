f98a07cac172d75301f4109c56988c9d116985de
HelloAyan <helloayan171@gmail.com>
2026-03-01 01:06:47 +0600

    fix: url changed

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ const app = express()
 
 // Middleware
 // app.use(cors())
-app.use(cors({
-    origin: "*",
-    credentials: true,
-}));
+app.use(cors());
 app.use(express.json())
 
 const PORT = process.env.PORT || 5000;
