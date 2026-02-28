fde92d1aa7809c07d022e76cd9f275fd328e197a
HelloAyan <helloayan171@gmail.com>
2026-03-01 00:40:26 +0600

    fix: modified

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 // Middleware
 // app.use(cors())
 app.use(cors({
-    origin: "https://todo-list-application-virid.vercel.app/",
+    origin: "https://todo-list-application-virid.vercel.app",
     credentials: true,
 }));
 app.use(express.json())
