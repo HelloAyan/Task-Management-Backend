c62ef7e71c95359049765872f2134cc41de0abf3
HelloAyan <helloayan171@gmail.com>
2026-03-04 14:12:07 +0600

    feat: new route added

--- controller/resetPassword.js
+++ controller/resetPassword.js
@@ -1,22 +1,22 @@
 import bcrypt from "bcryptjs";
 import crypto from "crypto";
-import User from "../model/registrationModel.js"
+import User from "../model/registrationModel.js";
+
 
 export const resetPassword = async (req, res) => {
     try {
         const { token } = req.params;
         const { password } = req.body;
 
-        // hash incoming token
         const hashedToken = crypto
-            .createHash("sha256")
+            .creatHash("sha256")
             .update(token)
             .digest("hex");
 
-        // find user by token + check expiry
+        // Find user by  token + check expiry
         const user = await User.findOne({
             resetPasswordToken: hashedToken,
-            resetPasswordExpire: { $gt: Date.now() }
+            resetPasswordExpire: { $gt: new Date() }
         });
 
         if (!user) {
@@ -25,10 +25,10 @@ export const resetPassword = async (req, res) => {
             });
         }
 
-        // hash new password
-        user.password = await bcrypt.hash(password, 10);
+        // has new password
+        user.password = await bcrypt.hash(password, 10)
 
-        // clear reset fields
+        // clear reset token fields
         user.resetPasswordToken = undefined;
         user.resetPasswordExpire = undefined;
 
@@ -37,11 +37,7 @@ export const resetPassword = async (req, res) => {
         res.status(200).json({
             message: "Password reset successful"
         });
-
-    } catch (error) {
-        res.status(500).json({
-            message: "Server error",
-            error: error.message
-        });
+    } catch { error } {
+        res.status(500).json({ message: "Server error" });
     }
-};
+}
--- route/userRoute.js
+++ route/userRoute.js
@@ -3,11 +3,15 @@ import { registerValidation } from "../validators/user.validator.js";
 import { validate } from "../middlewares/validate.middleware.js";
 import { registerUser } from "../controller/registrationController.js";
 import { LoginUser } from "../controller/loginController.js";
+import { forgetPassword } from "../controller/forgetPassword.js";
+import { resetPassword } from "../controller/resetPassword.js";
 
 
 const route = express.Router();
 
 route.post("/register", registerValidation, validate, registerUser)
 route.post("/login", LoginUser)
+route.post("/forget-password", forgetPassword)
+route.post("/reset-password/:token", resetPassword)
 
 export default route;
