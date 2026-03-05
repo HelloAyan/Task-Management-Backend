577dd27ec2d68561e99702ac8690cc0765f1f97d
HelloAyan <helloayan171@gmail.com>
2026-03-05 23:19:07 +0600

    update

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,31 +1,30 @@
-// utils/sendEmail.js
-
 import nodemailer from "nodemailer";
 
 const sendEmail = async (options) => {
 
-    // create SMTP transporter
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
+        port: Number(process.env.SMTP_PORT),
         secure: false,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
+        },
+        tls: {
+            rejectUnauthorized: false
         }
     });
 
-    // mail options
+    await transporter.verify();
+
     const mailOptions = {
-        from: `"Task Manager" <${process.env.SMTP_USER}>`,
+        from: `"Task Manager" <nirab2018@gmail.com>`,
         to: options.to,
         subject: options.subject,
         html: options.html,
     };
 
-    // send email
     await transporter.sendMail(mailOptions);
-
 };
 
 export default sendEmail;
